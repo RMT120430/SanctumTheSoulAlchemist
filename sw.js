6880dc0acb2a5189334c90d19b433899e059bf2f
RMT120430 <aifygnhuidsg8749832@gmail.com>
2026-04-15 17:50:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const CACHE_NAME = 'STSA v3.9.7';
+const CACHE_NAME = 'STSA v3.9.8';
 const urlsToCache = [
   '/SanctumTheSoulAlchemist/',
   '/SanctumTheSoulAlchemist/index.html',
   '/SanctumTheSoulAlchemist/styles.css',
-  '/SanctumTheSoulAlchemist/script.js'
+  '/SanctumTheSoulAlchemist/script.js',
+  '/SanctumTheSoulAlchemist/manifest.json',
+  '/SanctumTheSoulAlchemist/icon-192.png',
+  '/SanctumTheSoulAlchemist/icon-web.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js'
 ];
 
 self.addEventListener('install', event => {
@@ -22,6 +26,6 @@ self.addEventListener('fetch', event => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/SanctumTheSoulAlchemist/sw.js')
-    .then(reg => console.log('Service Worker 註冊成功 - sw.js:25'))
-    .catch(err => console.log('Service Worker 註冊失敗 - sw.js:26', err));
+    .then(reg => console.log('Service Worker 註冊成功 - sw.js:28'))
+    .catch(err => console.log('Service Worker 註冊失敗 - sw.js:29', err));
 }
